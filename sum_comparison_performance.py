d12b593803edf47bd1ff75372fc06659c05111c2
Daniel Buchan <clone@myself.com>
2016-06-21 18:14:01 +0100

    ch-ch-ch-changes

--- sum_comparison_performance.py
+++ sum_comparison_performance.py
@@ -177,7 +177,7 @@ pp.pprint(t1_results)
 def print_counts(top, data):
     output_line = ''
     for vect_count in sorted(data.items()):
-        pp.pprint(vect_count)
+        pp.pprint(vect_count[1])
         output_line += top+","+str(vect_count[0])+","
         output_line += str(round(vect_count[1]["class"]/150, 3))+","
         output_line += str(round(vect_count[1]["fold"]/150, 3))+","
