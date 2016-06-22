a045c97bea44ef61f9ebb26c0549dcd70bc5c01b
Daniel Buchan <clone@myself.com>
2016-06-22 15:08:08 +0100

    moved some stuff! !

--- count_comparison_perfomance.py
+++ count_comparison_perfomance.py
@@ -14,7 +14,9 @@ with open("/cs/research/bioinf/home1/green/dbuchan/archive0/eigen_thread/scop_da
         omit_from_results_set[line] = 1
         omit_from_results_set_pdb[line[1:5]+line[5:6].upper()]=1
 
-# pp.pprint(omit_from_results_set)
+
+# pp.pprint(omit_from_results_set_pdb)
+# exit()
 
 # get the list of scop folds
 scop_list = {}
@@ -23,27 +25,28 @@ with open("/cs/research/bioinf/home1/green/dbuchan/archive0/eigen_thread/scop_da
     reader = csv.reader(scop_list_file, delimiter=',', quotechar='"')
     for row in reader:
         #print(row)
-        scop_list[row[0]] = row[1]
+        scop_list[row[0]] = r".".join(row[1].split(".")[:-2])
         pdb = row[0][1:5]
         chain = row[0][5:6].upper()
         pdb_id = pdb+chain
         try:
-            pdb_list[pdb_id].append(row[1])
+            pdb_list[pdb_id].append(r".".join(row[1].split(".")[:-2]))
         except:
-            pdb_list[pdb_id] = [row[1], ]
+            pdb_list[pdb_id] = [r".".join(row[1].split(".")[:-2]), ]
 
 # pp.pprint(scop_list)
 # pp.pprint(pdb_list)
-
+# exit()
 
 bench_membership = {}
 with open('/cs/research/bioinf/home1/green/dbuchan/archive0/eigen_thread/scop_data/benchmark_family_members.txt') as benchfile:
     reader = csv.reader(benchfile, delimiter=',', quotechar='"')
     for row in reader:
-        bench_membership[row[0]] = row[1]
+        bench_membership[row[0]] = r".".join(row[1].split(".")[:-2])
 
 # pp.pprint(bench_membership)
-#
+# exit()
+
 # Parse eigenresults
 #
 def parse_eigen(omit_from_results_set, scop_list, bench_membership):
@@ -90,7 +93,7 @@ def parse_hh(omit_from_results_set, scop_list, bench_membership):
     start_parse_re = re.compile(start_parse_pattern)
     end_parse_pattern = r"^No\s1"
     end_parse_re = re.compile(end_parse_pattern)
-
+    print("hi")
     for file in glob.glob(result_dir+"hhresults/*.hhr"):
         print(file)
         results_list = []
@@ -105,7 +108,7 @@ def parse_hh(omit_from_results_set, scop_list, bench_membership):
             out = open(result_dir+"processed_comparison/"+pdb_id+".hhtop", "w+")
             out.write("# PDB ID: "+pdb_id+"\n")
             out.write("# SCOP FAMILY: "+bench_membership[pdb_id]+"\n")
-            # print(file)
+            print(file)
             with open(file) as hhrResult:
                 lines = hhrResult.read().splitlines()
                 for line in lines:
@@ -146,7 +149,7 @@ def parse_hh(omit_from_results_set, scop_list, bench_membership):
                 # pp.pprint(element)
         else:
             continue
-        #break
+        # break
 
 def parse_genth(omit_from_results_set_pbd, pdb_list, bench_membership):
     result_dir = "/cs/research/bioinf/home1/green/dbuchan/archive0/eigen_thread/results/"
@@ -187,6 +190,6 @@ def parse_genth(omit_from_results_set_pbd, pdb_list, bench_membership):
             continue
         # break
 
-parse_eigen(omit_from_results_set, scop_list, bench_membership)
+# parse_eigen(omit_from_results_set, scop_list, bench_membership)
 parse_hh(omit_from_results_set, scop_list, bench_membership)
-parse_genth(omit_from_results_set_pdb, pdb_list, bench_membership)
+# parse_genth(omit_from_results_set_pdb, pdb_list, bench_membership)
