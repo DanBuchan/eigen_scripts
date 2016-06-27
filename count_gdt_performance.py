dca6aab689cb00931aea5a1cf16db66cc9836234
Daniel Buchan <clone@myself.com>
2016-06-27 16:43:13 +0100

    things

--- count_gdt_performance.py
+++ count_gdt_performance.py
@@ -12,9 +12,11 @@ def skip_comments(iterable):
 result_dir = "/cs/research/bioinf/home1/green/dbuchan/archive0/eigen_thread/" \
              "results/processed_comparison/"
 eigen_models = "/cs/research/bioinf/home1/green/dbuchan/archive0/" \
-               "eigen_thread/results/optimised/models/"
+               "eigen_thread/results/optimised_t20_c9/models/"
+
 for file in glob.glob(result_dir+"*.eigentop"):
     pdb_id = file[-14:-9]
+    print(file)
     with open(file) as scop_list_file:
         reader = csv.reader(skip_comments(scop_list_file), delimiter=',',
                             quotechar='"')
