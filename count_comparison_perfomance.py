381a57fdfe303d5b3fdf7167c7a2f089428d2477
Daniel Buchan <clone@myself.com>
2016-06-27 17:08:32 +0100

    things

--- count_comparison_perfomance.py
+++ count_comparison_perfomance.py
@@ -85,9 +85,9 @@ def parse_eigen(omit_from_results_set, scop_list, bench_membership):
                         this_3_levels = ".".join(scop_list[line[3]].split(".")[:-1])
                         # print(scop_3_levels)
                         if scop_list[line[3]] == scop_class:
-                            print("FAMILY MATCH")
+                            # print("FAMILY MATCH")
                         elif scop_3_levels == this_3_levels:
-                            print("SUPERFAMILY MATCH")
+                            # print("SUPERFAMILY MATCH")
                         else:
                             result_array = [line[0], line[3], scop_list[line[3]]]
                     except:
