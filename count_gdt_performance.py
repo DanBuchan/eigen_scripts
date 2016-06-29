e68e4177984b1609a19e1e059b2222c77d3cab9b
Daniel Buchan <clone@myself.com>
2016-06-29 12:32:01 +0100

    gdt done

--- count_gdt_performance.py
+++ count_gdt_performance.py
@@ -1,3 +1,5 @@
+
+from __future__ import print_function
 import glob
 import pprint
 import csv
@@ -5,11 +7,17 @@ import re
 from subprocess import Popen, PIPE
 import shlex
 from statistics import mean
+import sys
+
 
 def skip_comments(iterable):
     for line in iterable:
         if not line.startswith('#'):
             yield line
+
+
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
 
 result_dir = "/cs/research/bioinf/home1/green/dbuchan/archive0/eigen_thread/" \
              "results/processed_comparison/"
@@ -19,18 +27,25 @@ benchmark_models = "/cs/research/bioinf/home1/green/dbuchan/archive0/" \
                    "eigen_thread/eigenthreader/structures/"
 
 
-def average_eigen_scores(result_dir, ending):
-    tm_pattern = "TM-score\s+=\s+(.+)\s+\("
-    gdt_pattern = "GDT=\s+(.+?)"
+def average_scores(result_dir, ending):
+
+    if "eigentop" in ending:
+        models_dir = "/cs/research/bioinf/home1/green/dbuchan/archive0/" \
+                     "eigen_thread/results/optimised_t20_c9/models/"
+    if "genthtop" in ending:
+        models_dir = "/cs/research/bioinf/home1/green/dbuchan/archive0/" \
+                     "eigen_thread/results/genthreader_results/models/"
+
+    tm_pattern = "\nTM-score=\s+(.+?)\s+\("
+    gdt_pattern = "\nGDT=\s+(.+)"
     tm_re = re.compile(tm_pattern)
     gdt_re = re.compile(gdt_pattern)
 
-    totes = 150
     tm_averages = [[], [], [], []]
     gdt_averages = [[], [], [], []]
     for file in glob.glob(result_dir+ending):
         pdb_id = file[-14:-9]
-        print(file)
+        eprint(file)
         with open(file) as scop_list_file:
             reader = csv.reader(skip_comments(scop_list_file), delimiter=',',
                                 quotechar='"')
@@ -40,31 +55,37 @@ def average_eigen_scores(result_dir, ending):
                 model = pdb_id+"_"+line[1]+".model.pdb"
                 native_struct = pdb_id.upper()[0:4]+"_"+pdb_id.upper()[4:5]+".pdb"
                 # print(benchmark_models+native_struct)
-                # print(eigen_models+model)
-                cmd = "/cs/research/bioinf/home1/green/dbuchan/bin/TMalign " + \
-                    eigen_models+model+" " + \
-                    benchmark_models+native_struct
-                print(cmd)
-                process = Popen(shlex.split(cmd), stdout=PIPE)
-                (output, err) = process.communicate()
-                exit_code = process.wait()
-                tm_result = tm_re.search(output.decode("utf-8"))
+                # print(models_dir+model)
+                try:
+                    cmd = "/cs/research/bioinf/home1/green/dbuchan/bin/TMalign " + \
+                        models_dir+model+" " + \
+                        benchmark_models+native_struct
+                    # print(cmd)
+                    process = Popen(shlex.split(cmd), stdout=PIPE)
+                    (output, err) = process.communicate()
+                    #print(output.decode("utf-8"))
+                    exit_code = process.wait()
+                    tm_result = tm_re.search(output.decode("utf-8"))
+                    tm_results.append(float(tm_result.group(1)))
+                except:
+                    eprint("COULD NOT RUN TMalign")
 
-                cmd = "/cs/research/bioinf/home1/green/dbuchan/bin/maxcluster" + \
-                    " -e "+eigen_models+model + \
-                    " -p "+benchmark_models+native_struct + \
-                    " -in -gdt"
-                print(cmd)
-                process = Popen(shlex.split(cmd), stdout=PIPE)
-                (output, err) = process.communicate()
-                exit_code = process.wait()
-                gdt_result = gdt_re.search(output.decode("utf-8"))
+                try:
+                    cmd = "/cs/research/bioinf/home1/green/dbuchan/bin/maxcluster64bit" + \
+                        " -e "+models_dir+model + \
+                        " -p "+benchmark_models+native_struct + \
+                        " -in -gdt"
+                    # print(cmd)
+                    process = Popen(shlex.split(cmd), stdout=PIPE)
+                    (output, err) = process.communicate()
+                    exit_code = process.wait()
+                    gdt_result = gdt_re.search(output.decode("utf-8"))
+                    gdt_results.append(float(gdt_result.group(1)))
+                except:
+                    eprint("COULD NOT RUN maxcluster")
 
-                tm_results.append(float(tm_result.group(1)))
-                gdt_results.append(float(gdt_result.group(1)))
-
-            print(tm_results)
-            print(gdt_results)
+            # print(tm_results)
+            # print(gdt_results)
 
             if len(tm_results) > 0:
                 tm_averages[0].append(tm_results[0])
@@ -86,8 +107,65 @@ def average_eigen_scores(result_dir, ending):
 
     return(tm_averages, gdt_averages)
 
-(eigen_tm_averages, eigen_gdt_averages) = average_eigen_scores(result_dir,
-                                                               "*.eigentop")
+(eigen_tm_averages, eigen_gdt_averages) = average_scores(result_dir,
+                                                         "*.eigentop")
+(gen_tm_averages, gen_gdt_averages) = average_scores(result_dir,
+                                                     "*.genthtop")
 
-print(eigen_tm_averages)
-print(eigen_gdt_averages)
+tm_results = []
+gdt_results = []
+tm_pattern = "\nTM-score=\s+(.+?)\s+\("
+gdt_pattern = "\nGDT=\s+(.+)"
+tm_re = re.compile(tm_pattern)
+gdt_re = re.compile(gdt_pattern)
+for file in glob.glob("/cs/research/bioinf/home1/green/dbuchan/archive0/eigen_thread/results/hhresults/models/"+"*.pdb"):
+    pdb_id = file[-9:-4]
+    native_struct = pdb_id.upper()[0:4]+"_"+pdb_id.upper()[4:5]+".pdb"
+    try:
+        cmd = "/cs/research/bioinf/home1/green/dbuchan/bin/TMalign " + \
+            file+" " + \
+            benchmark_models+native_struct
+        # print(cmd)
+        process = Popen(shlex.split(cmd), stdout=PIPE)
+        (output, err) = process.communicate()
+        #print(output.decode("utf-8"))
+        exit_code = process.wait()
+        tm_result = tm_re.search(output.decode("utf-8"))
+        tm_results.append(float(tm_result.group(1)))
+    except:
+        eprint("COULD NOT RUN TMalign")
+
+    try:
+        cmd = "/cs/research/bioinf/home1/green/dbuchan/bin/maxcluster64bit" + \
+            " -e "+file + \
+            " -p "+benchmark_models+native_struct + \
+            " -in -gdt"
+        # print(cmd)
+        process = Popen(shlex.split(cmd), stdout=PIPE)
+        (output, err) = process.communicate()
+        exit_code = process.wait()
+        gdt_result = gdt_re.search(output.decode("utf-8"))
+        gdt_results.append(float(gdt_result.group(1)))
+    except:
+        eprint("COULD NOT RUN maxcluster")
+    #break
+
+print("level,eigen_average_tm,eigen_average_gdt,"
+      "gen_average_tm,gen_average_gdt,hh_average_tm,hh_average_gdt")
+print("t1,"+str(round(mean(eigen_tm_averages[0]), 2))+"," +
+      str(round(mean(eigen_gdt_averages[0]), 2))+"," +
+      str(round(mean(gen_tm_averages[0]), 2))+"," +
+      str(round(mean(gen_gdt_averages[0]), 2))+",-,-")
+print("t2,"+str(round(mean(eigen_tm_averages[1]), 2))+"," +
+      str(round(mean(eigen_gdt_averages[1]), 2))+"," +
+      str(round(mean(gen_tm_averages[1]), 2))+"," +
+      str(round(mean(gen_gdt_averages[1]), 2))+",-,-")
+print("t5,"+str(round(mean(eigen_tm_averages[2]), 2))+"," +
+      str(round(mean(eigen_gdt_averages[2]), 2))+"," +
+      str(round(mean(gen_tm_averages[2]), 2))+"," +
+      str(round(mean(gen_gdt_averages[2]), 2))+",-,-")
+print("t10,"+str(round(mean(eigen_tm_averages[3]), 2))+"," +
+      str(round(mean(eigen_gdt_averages[3]), 2))+"," +
+      str(round(mean(gen_tm_averages[3]), 2))+"," +
+      str(round(mean(gen_gdt_averages[3]), 2))+"," +
+      str(round(mean(tm_results), 2))+","+str(round(mean(gdt_results), 2)))
