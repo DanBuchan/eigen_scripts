9703d19c8b2e63f762a93de9c476de57ba647e7e
Daniel Buchan <clone@myself.com>
2016-08-22 13:13:46 +0100

    adjust count_perf

--- count_performance.py
+++ count_performance.py
@@ -9,7 +9,7 @@ import math
 from collections import defaultdict
 pp = pprint.PrettyPrinter(indent=4)
 
-result_dir = "/cs/research/bioinf/home1/green/dbuchan/archive0/eigen_thread/results/distance"
+result_dir = "/cs/research/bioinf/home1/green/dbuchan/archive0/eigen_thread/results/random"
 pdb_pattern = r"#\sPDB\sID:\s(.+)"
 scop_pattern = r"#\sSCOP\sFAMILY:\s(.+)"
 result_pattern = r".+,.+,(.+)"
@@ -27,9 +27,9 @@ t2_results = defaultdict(dict)
 t5_results = defaultdict(dict)
 t10_results = defaultdict(dict)
 
-for directory in glob.glob(result_dir+"/c*"):
+for directory in glob.glob(result_dir+"/L*"):
     param = directory[-3:]
-    param = int(param.lstrip("/c"))
+    param = int(param.lstrip("/L"))
     t1_results[param]["class"] = 0
     t2_results[param]["class"] = 0
     t5_results[param]["class"] = 0
