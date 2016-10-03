7b0807f302f83678ec5bc573004faf8b3862d538
Daniel Buchan <clone@myself.com>
2016-10-03 16:56:28 +0100

    changed parsing of other method structural hits

--- count_gdt_performance.py
+++ count_gdt_performance.py
@@ -146,6 +146,9 @@ print(hh_gdt_averages)
 
 print("level,eigen_average_tm,eigen_average_gdt,"
       "gen_average_tm,gen_average_gdt,hh_average_tm,hh_average_gdt")
+print(len(hh_tm_averages[0]))
+print(len(hh_gdt_averages[0]))
+
 print("t1,"+str(round(mean(eigen_tm_averages[0]), 2))+"," +
       str(round(mean(eigen_gdt_averages[0]), 2))+"," +
       str(round(mean(gen_tm_averages[0]), 2))+"," +
