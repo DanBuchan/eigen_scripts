13d02ceec1557ed4d5ea80e49a2a6d8c23a72bf6
Daneil Buchan <clone@myself.com>
2016-12-21 15:29:22 +0000

    made R figures more legible

--- draw_comparison.R
+++ draw_comparison.R
@@ -16,9 +16,9 @@ colnames(sf_comparison_data)<-c("Top","Method","Class","Fold")
 sf_comparison_data$Top<-as.factor(sf_comparison_data$Top)
 sf_comparison_data$Method<-as.factor(sf_comparison_data$Method)
 
-p1<-ggplot(comparison_data, aes(x=Top, y=Class, group=Method, fill=Method))+geom_bar(stat="identity", position="dodge")+ggtitle("Comparison of Class Recognition Performance")+ylab("TPR")+xlab("Top X")+theme(plot.title= element_text(hjust=0.5),text=element_text(size=25))
-p2<-ggplot(comparison_data, aes(x=Top, y=Fold, group=Method, fill=Method))+geom_bar(stat="identity", position="dodge")+ggtitle("Superfamily and family hits excluded")+ylab("TPR")+xlab("Top X")+theme(plot.title= element_text(hjust=0.5),text=element_text(size=25))
+p1<-ggplot(comparison_data, aes(x=Top, y=Class, group=Method, fill=Method))+geom_bar(stat="identity", position="dodge")+ggtitle("Comparison of Class Recognition Performance")+ylab("TPR")+xlab("Top X")+theme(plot.title= element_text(hjust=0.5),text=element_text(size=25))+theme_set(theme_gray(base_size = 25))
+p2<-ggplot(comparison_data, aes(x=Top, y=Fold, group=Method, fill=Method))+geom_bar(stat="identity", position="dodge")+ggtitle("Superfamily and family hits excluded")+ylab("TPR")+xlab("Top X")+theme(plot.title= element_text(hjust=0.5),text=element_text(size=25))+theme_set(theme_gray(base_size = 25))
 
-p3<-ggplot(sf_comparison_data, aes(x=Top, y=Class, group=Method, fill=Method))+geom_bar(stat="identity", position="dodge")+ggtitle("Comparison of Class Recognition Performance")+ylab("TPR")+xlab("Top X")+theme(plot.title= element_text(hjust=0.5),text=element_text(size=25))
-p4<-ggplot(sf_comparison_data, aes(x=Top, y=Fold, group=Method, fill=Method))+geom_bar(stat="identity", position="dodge")+ggtitle("Family hits excluded")+ylab("TPR")+xlab("Top X")+theme(plot.title= element_text(hjust=0.5),text=element_text(size=25))
+p3<-ggplot(sf_comparison_data, aes(x=Top, y=Class, group=Method, fill=Method))+geom_bar(stat="identity", position="dodge")+ggtitle("Comparison of Class Recognition Performance")+ylab("TPR")+xlab("Top X")+theme(plot.title= element_text(hjust=0.5),text=element_text(size=25))+theme_set(theme_gray(base_size = 25))
+p4<-ggplot(sf_comparison_data, aes(x=Top, y=Fold, group=Method, fill=Method))+geom_bar(stat="identity", position="dodge")+ggtitle("Family hits excluded")+ylab("TPR")+xlab("Top X")+theme(plot.title= element_text(hjust=0.5),text=element_text(size=25))+theme_set(theme_gray(base_size = 25))
 multiplot(p4,p2,cols=2)
