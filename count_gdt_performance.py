ef3e8c3ed896b28271c906e6d88aec96895d7d93
Daniel Buchan <clone@myself.com>
2017-01-09 18:28:33 +0000

    fixing TPR calc

--- count_gdt_performance.py
+++ count_gdt_performance.py
@@ -43,111 +43,6 @@ def read_missing(file):
             missing_things.append(line)
     return(missing_things)
 
-def average_scores(result_dir, ending):
-
-    if "eigentop" in ending:
-        models_dir = "/mnt/bioinf/archive0/" \
-                     "eigen_thread/results/optimised_t20_c9/models/"
-    if "genthtop" in ending:
-        models_dir = "/mnt/bioinf/archive0/" \
-                     "eigen_thread/results/genthreader_results/models/"
-    if "hhtop" in ending:
-        models_dir = "/mnt/bioinf/archive0/" \
-                     "eigen_thread/results/hhresults/models/"
-
-    missing_things = read_missing("/home/dbuchan/Code/eigen_scripts/"
-                                  "missing_hh_models.txt")
-
-    tm_pattern = "TM-Score\s=\s(.+?)\sRMSD"
-    gdt_pattern = "GDT-TS\s=\s(.+)\sTM"
-    tm_re = re.compile(tm_pattern)
-    gdt_re = re.compile(gdt_pattern)
-
-    tm_averages = [[], [], [], []]
-    gdt_averages = [[], [], [], []]
-    pdb_id = ''
-    for file in glob.glob(result_dir+ending):
-        if "eigentop" in ending or "genthtop" in ending:
-            pdb_id = file[-14:-9]
-        else:
-            pdb_id = file[-11:-6]
-        # fasta_file = "/mnt/bioinf/archive0/" \
-        #              "eigen_thread/eigenthreader/seq_files/"+pdb_id+".fasta"
-        #fasta_length = getFastaLength(fasta_file)
-        #print(pdb_id)
-        if pdb_id in missing_things:
-            print("MISSING")
-            continue
-        # print(pdb_id)
-        # continue
-        eprint(file)
-        with open(file) as scop_list_file:
-            reader = csv.reader(skip_comments(scop_list_file), delimiter=',',
-                                quotechar='"')
-            tm_results = []
-            gdt_results = []
-            for line in reader:
-                if len(line[1]) == 0:
-                    continue
-                model = pdb_id+"_"+line[1]+".model.pdb"
-                native_struct = pdb_id.upper()[0:4]+"_"+pdb_id.upper()[4:5]+".pdb"
-                eprint(benchmark_models+native_struct)
-                eprint(models_dir+model)
-                # try:
-                #     cmd = "/home/dbuchan/bin/TMalign " + \
-                #         models_dir+model+" " + \
-                #         benchmark_models+native_struct
-                #     eprint(cmd)
-                #     process = Popen(shlex.split(cmd), stdout=PIPE)
-                #     (output, err) = process.communicate()
-                #     #print(output.decode("utf-8"))
-                #     exit_code = process.wait()
-                #     tm_result = tm_re.search(output.decode("utf-8"))
-                #     tm_results.append(float(tm_result.group(1)))
-                # except:
-                #     eprint("COULD NOT RUN TMalign")
-
-                try:
-                    cmd = "/home/dbuchan/bin/gdtlist " + \
-                          benchmark_models+native_struct + \
-                          " "+models_dir+model
-                    eprint(cmd)
-                    process = Popen(shlex.split(cmd), stdout=PIPE)
-                    (output, err) = process.communicate()
-                    exit_code = process.wait()
-                    gdt_result = gdt_re.search(output.decode("utf-8"))
-                    gdt_results.append(float(gdt_result.group(1)))
-                    tm_result = tm_re.search(output.decode("utf-8"))
-                    tm_results.append(float(tm_result.group(1)))
-                except Exception as e:
-                    eprint("COULD NOT RUN gdtlist")
-                    eprint(str(e))
-            #exit()
-
-            # print(tm_results)
-            # print(gdt_results)
-
-            if len(tm_results) > 0:
-                tm_averages[0].append(tm_results[0])
-            if len(tm_results) > 1:
-                tm_averages[1].append(max(tm_results[0:2]))
-            if len(tm_results) > 4:
-                tm_averages[2].append(max(tm_results[0:5]))
-            if len(tm_results) > 9:
-                tm_averages[3].append(max(tm_results))
-
-            if len(gdt_results) > 0:
-                gdt_averages[0].append(gdt_results[0])
-            if len(gdt_results) > 1:
-                gdt_averages[1].append(max(gdt_results[0:2]))
-            if len(gdt_results) > 4:
-                gdt_averages[2].append(max(gdt_results[0:5]))
-            if len(gdt_results) > 9:
-                gdt_averages[3].append(max(gdt_results))
-        # break
-
-    return(tm_averages, gdt_averages)
-
 def calculate_structure_scores(result_dir, ending):
 
     if "eigentop" in ending:
@@ -196,19 +91,6 @@ def calculate_structure_scores(result_dir, ending):
             native_struct = pdb_id.upper()[0:4]+"_"+pdb_id.upper()[4:5]+".pdb"
             eprint(benchmark_models+native_struct)
             eprint(models_dir+model)
-            # try:
-            #     cmd = "/home/dbuchan/bin/TMalign " + \
-            #         models_dir+model+" " + \
-            #         benchmark_models+native_struct
-            #     eprint(cmd)
-            #     process = Popen(shlex.split(cmd), stdout=PIPE)
-            #     (output, err) = process.communicate()
-            #     #print(output.decode("utf-8"))
-            #     exit_code = process.wait()
-            #     tm_result = tm_re.search(output.decode("utf-8"))
-            #     tm_results.append(float(tm_result.group(1)))
-            # except:
-            #     eprint("COULD NOT RUN TMalign")
 
             try:
                 cmd = "/home/dbuchan/bin/gdtlist " + \
@@ -222,17 +104,17 @@ def calculate_structure_scores(result_dir, ending):
                 gdt_result = gdt_re.search(output.decode("utf-8"))
                 tm_result = tm_re.search(output.decode("utf-8"))
                 if line_count == 1:
-                    tm_scores[1].append(float(tm_result.group(1))
-                    gdt_scores[1].append(float(gdt_result.group(1))
+                    tm_scores[1].append(float(tm_result.group(1)))
+                    gdt_scores[1].append(float(gdt_result.group(1)))
                 if line_count <= 2:
-                    tm_scores[2].append(float(tm_result.group(1))
-                    gdt_scores[2].append(float(gdt_result.group(1))
+                    tm_scores[2].append(float(tm_result.group(1)))
+                    gdt_scores[2].append(float(gdt_result.group(1)))
                 if line_count <= 5:
-                    tm_scores[5].append(float(tm_result.group(1))
-                    gdt_scores[5].append(float(gdt_result.group(1))
+                    tm_scores[5].append(float(tm_result.group(1)))
+                    gdt_scores[5].append(float(gdt_result.group(1)))
                 if line_count <= 10:
-                    tm_scores[10].append(float(tm_result.group(1))
-                    gdt_scores[10].append(float(gdt_result.group(1))
+                    tm_scores[10].append(float(tm_result.group(1)))
+                    gdt_scores[10].append(float(gdt_result.group(1)))
             except Exception as e:
                 eprint("COULD NOT RUN gdtlist")
                 eprint(str(e))
