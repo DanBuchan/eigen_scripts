036447b267dd29f0f13c348f41af544bc747e359
Daneil Buchan <clone@myself.com>
2017-04-21 10:44:01 +0100

    updated pallette for greyscale and colourblind safe for barchart

--- eigen_hh_structure_comparison.R
+++ eigen_hh_structure_comparison.R
@@ -4,4 +4,4 @@ library(Rmisc)
 tm_comparison <- read.csv(file="/mnt/bioinf/archive0/eigen_thread/results/t1_tm_eigen_hh_comparison.csv", header=TRUE, check.names=FALSE, strip.white = TRUE, sep=",",na.strings= c("999", "NA", " ", ""))
 scatter_data <- dcast(tm_comparison, formula = pdb~method, value.var="score", fill=0)
 
-ggplot(scatter_data, aes(x=hh, y=eigen))+geom_point(shape=16, size=5)+geom_abline(colour="grey")+scale_y_continuous(limits=c(0,1))+scale_x_continuous(limits=c(0,1))+ylab("EigenThreader")+xlab("HHSearch")+theme(axis.text=element_text(size=20), )
+ggplot(scatter_data, aes(x=hh, y=eigen))+geom_point(shape=16, size=5)+geom_abline(colour="grey")+scale_y_continuous(limits=c(0,1))+scale_x_continuous(limits=c(0,1))+ylab("EigenTHREADER TM-score")+xlab("HHSearch TM-score")+theme(axis.text=element_text(size=20),axis.title=element_text(size=20) )
