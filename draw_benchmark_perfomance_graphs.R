f273a141721d4035c4ad2203e8d256d60953bda7
Daneil Buchan <clone@myself.com>
2017-04-21 12:36:00 +0100

    updated figure 2 and 3 for clearer b&w printing

--- draw_benchmark_perfomance_graphs.R
+++ draw_benchmark_perfomance_graphs.R
@@ -12,7 +12,67 @@ v1<-ggplot(vector_performance, aes(x=vectors, y=class, group=top, color=top))+ge
 v2<-ggplot(vector_performance, aes(x=vectors, y=fold, group=top, color=top))+geom_line(size=0.8)+theme_set(theme_gray(base_size = 25)) + ggtitle("Fold")+ylab("TPR")+xlab("No. of Eigenvectors")+theme(plot.title= element_text(hjust=0.5))+scale_y_continuous(limits=c(0.25,1))
 v3<-ggplot(vector_performance, aes(x=vectors, y=superf, group=top, color=top))+geom_line(size=0.8)+theme_set(theme_gray(base_size = 25)) + ggtitle("Superfamily")+ylab("TPR")+xlab("No. of Eigenvectors")+theme(plot.title= element_text(hjust=0.5))+scale_y_continuous(limits=c(0.25,1))
 multiplot(v1,v3,v2,cols=2)
+
 d1<-ggplot(distance_performance, aes(x=distance, y=class, group=top, color=top))+geom_line(size=0.8)+theme_set(theme_gray(base_size = 25)) + ggtitle("Class")+ylab("TPR")+xlab("Distance (angstroms)")+theme(plot.title= element_text(hjust=0.5))+scale_y_continuous(limits=c(0,1))
 d2<-ggplot(distance_performance, aes(x=distance, y=fold, group=top, color=top))+geom_line(size=0.8)+theme_set(theme_gray(base_size = 25)) + ggtitle("Fold")+ylab("TPR")+xlab("Distance (angstroms)")+theme(plot.title= element_text(hjust=0.5))+scale_y_continuous(limits=c(0,1))
 d3<-ggplot(distance_performance, aes(x=distance, y=superf, group=top, color=top))+geom_line(size=0.8)+theme_set(theme_gray(base_size = 25)) + ggtitle("Superfamily")+ylab("TPR")+xlab("Distance (angstroms)")+theme(plot.title= element_text(hjust=0.5))+scale_y_continuous(limits=c(0,1))
 multiplot(d1,d3,d2,cols=2)
+
+vect_class<-vector_performance
+vect_class$fold<-NULL
+vect_class$superf<-NULL
+vect_class$family<-NULL
+colnames(vect_class)[3]<-"value"
+vect_class$label="Class"
+head(vect_class)
+
+vect_fold<-vector_performance
+vect_fold$class<-NULL
+vect_fold$superf<-NULL
+vect_fold$family<-NULL
+colnames(vect_fold)[3]<-"value"
+vect_fold$label="Fold"
+head(vect_fold)
+
+vect_superf<-vector_performance
+vect_superf$class<-NULL
+vect_superf$fold<-NULL
+vect_superf$family<-NULL
+colnames(vect_superf)[3]<-"value"
+vect_superf$label="Superfamily"
+head(vect_superf)
+
+whole_performance<-rbind(vect_class,vect_fold,vect_superf)
+whole_performance
+
+ggplot(whole_performance, aes(x=vectors, y=value, group=top, color=top))+geom_line(size=0.8)+geom_point(aes(shape=top), size=4)+theme_set(theme_gray(base_size = 15)) + ylab("TPR")+xlab("No. of Eigenvectors")+theme(strip.text.x=element_text(size=15), strip.background = element_rect(colour="white", fill="white"), axis.line = element_line(), panel.background=element_blank(), plot.title= element_text(hjust=0.5), legend.key.size=unit(2, "cm"))+scale_y_continuous(limits=c(0.25,1))+facet_wrap(~label, scales="free",ncol=2)
+ggsave("/home/dbuchan/Dropbox/EigenThreader/proofed_figures/figure_2_eigenvector_performance.png", width=10, height=7, dpi=300)
+
+dist_class<-distance_performance
+dist_class$fold<-NULL
+dist_class$superf<-NULL
+dist_class$family<-NULL
+colnames(dist_class)[3]<-"value"
+dist_class$label="Class"
+head(dist_class)
+
+dist_fold<-distance_performance
+dist_fold$class<-NULL
+dist_fold$superf<-NULL
+dist_fold$family<-NULL
+colnames(dist_fold)[3]<-"value"
+dist_fold$label="Fold"
+head(dist_fold)
+
+dist_superf<-distance_performance
+dist_superf$class<-NULL
+dist_superf$fold<-NULL
+dist_superf$family<-NULL
+colnames(dist_superf)[3]<-"value"
+dist_superf$label="Superfamily"
+head(dist_superf)
+
+whole_distance<-rbind(dist_class,dist_fold,dist_superf)
+whole_distance
+ggplot(whole_distance, aes(x=distance, y=value, group=top, color=top))+geom_line(size=0.8)+geom_point(aes(shape=top), size=4)+theme_set(theme_gray(base_size = 15)) + ylab("TPR")+xlab("No. of Eigenvectors")+theme(strip.text.x=element_text(size=15), strip.background = element_rect(colour="white", fill="white"), axis.line = element_line(), panel.background=element_blank(), plot.title= element_text(hjust=0.5), legend.key.size=unit(2, "cm"))+scale_y_continuous(limits=c(0,1))+facet_wrap(~label, scales="free",ncol=2)
+ggsave("/home/dbuchan/Dropbox/EigenThreader/proofed_figures/figure_3_distance_performance.png", width=10, height=7, dpi=300)
--- draw_comparison.R
+++ draw_comparison.R
@@ -22,7 +22,7 @@ p2<-ggplot(comparison_data, aes(x=Top, y=Fold, group=Method, fill=Method))+geom_
 
 p3<-ggplot(sf_comparison_data, aes(x=Top, y=Class, group=Method, fill=Method))+geom_bar(stat="identity", position="dodge")+scale_fill_brewer(palette='Blues')+scale_colour_brewer(palette='Blues')+ggtitle("Comparison of Class Recognition Performance")+ylab("TPR")+xlab("Top X")+theme(plot.title= element_text(hjust=0.5),text=element_text(size=25))
 p4<-ggplot(sf_comparison_data, aes(x=Top, y=Fold, group=Method, fill=Method))+geom_bar(stat="identity", position="dodge", aes(colour = Method))+scale_fill_brewer(palette='Blues')+scale_colour_brewer(palette='Blues')+ylab("TPR")+xlab("Top X")+theme(legend.position="none", plot.title= element_text(hjust=0.5),text=element_text(size=25))
-multiplot(p4,p2,cols=2)
+#multiplot(p4,p2,cols=2)
 
 # plot_grid(p4,p2,nrow=1,ncol=2, rel_widths= c(1/3,1/2), label_size = 20, labels=c("Family hits excluded","Superfamily and family hits excluded"))
 comp_data_with_type <- comparison_data
@@ -30,4 +30,5 @@ comp_data_with_type$Label = "Superfamily & family hits excluded"
 sf_data_with_type<- sf_comparison_data
 sf_data_with_type$Label = "Family hits excluded"
 whole_data<-rbind(sf_data_with_type,comp_data_with_type)
-ggplot(whole_data, aes(x=Top, y=Fold, group=Method, fill=Method))+geom_bar(stat="identity", position="dodge", aes(colour = Method))+scale_fill_brewer(palette='Blues')+scale_colour_brewer(palette='Blues')+ylab("TPR")+xlab("Top X")+theme(strip.text.x=element_text(size=25), strip.background = element_rect(colour="white", fill="white"), axis.line = element_line(), panel.background=element_blank(), plot.title= element_text(hjust=0.5),text=element_text(size=25))+facet_wrap(~Label, scales="free")+scale_y_continuous(limits=c(0,1))
+ggplot(whole_data, aes(x=Top, y=Fold, group=Method, fill=Method))+geom_bar(stat="identity", position="dodge", aes(colour = Method))+scale_fill_brewer(palette='Blues')+scale_colour_brewer(palette='Blues')+ylab("TPR")+xlab("Top X")+theme(strip.text.x=element_text(size=15), strip.background = element_rect(colour="white", fill="white"), axis.line = element_line(), panel.background=element_blank(), plot.title= element_text(hjust=0.5),text=element_text(size=15))+facet_wrap(~Label, scales="free")+scale_y_continuous(limits=c(0,1))
+ggsave("/home/dbuchan/Dropbox/EigenThreader/proofed_figures/figure_5_TPR.png", width=10, height=7, dpi=300)
